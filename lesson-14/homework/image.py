3af89f74bfe354141efad0e75e9fd6f769dc6ea9
aslbekrakhmatov-org <aslbek.rakhmatov.us@gmail.com>
2025-03-10 09:26:03 +0500

    lesson 14 again

--- lesson-14/homework/image.py
+++ lesson-14/homework/image.py
@@ -13,7 +13,7 @@ def flip_image(img):
     flipped_rl_img = Image.fromarray(flipped_rl_arr, mode="RGB")
     flipped_rl_img.save("Python_Homework/lesson-14/homework/images/rightleft_flipped.jpg")
 
-# flip_image("Python_Homework/lesson-14/homework/images/birds.jpg")
+flip_image("Python_Homework/lesson-14/homework/images/birds.jpg")
 
 #random noise
 def make_noisy_image(img):
@@ -25,7 +25,7 @@ def make_noisy_image(img):
     noisy_image_arr = np.clip(image_arr + noise_arr, 0, 255).astype(np.uint8)
     noisy_image = Image.fromarray(noisy_image_arr)
     noisy_image.save("Python_Homework/lesson-14/homework/images/noisy_image.jpg")
-#make_noisy_image("Python_Homework/lesson-14/homework/images/birds.jpg")
+make_noisy_image("Python_Homework/lesson-14/homework/images/birds.jpg")
 
 # brighten channels
 def brighten_channels(img):
@@ -35,7 +35,7 @@ def brighten_channels(img):
     brighter_img_arr = image_arr.astype(np.uint8)
     brighter_img = Image.fromarray(brighter_img_arr, mode="RGB")
     brighter_img.save("Python_Homework/lesson-14/homework/images/brighter_image.jpg")
-#brighten_channels("Python_Homework/lesson-14/homework/images/brighter_image.jpg")
+brighten_channels("Python_Homework/lesson-14/homework/images/brighter_image.jpg")
 
 # mask
 def mask_make_in_image(img):
